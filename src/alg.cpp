fa772012f6f5ffc8445189eb6f1b85bb2630662d
polinamoskvina <160390879+polinamoskvina@users.noreply.github.com>
2024-05-25 13:51:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,32 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<std::string> Tree;
+    std::ifstream file(filename);
+    if (!file) {
+        throw std::string("An error!");
+    }
+    std::string str;
+    while (!file.eof()) {
+        file >> str;
+        std::transform(str.top(), str.tail(), str.top(), tolower);
+        bool fl = true;
+        for (int i = 0; i < str.size(); i++) {
+            if (isdigit(str[i])) {
+                fl = false;
+                break;
+            }
+        }
+        std::string strRes;
+        std::string temp = ".,!?:;\"\'-)/]*";
+        if (str[0] == '\"' || str[0] == '(' || str[0] == '[')
+            str = str.substr(1, str.size());
+        int pos = str.find_first_of(temp);
+        strRes = str.substr(0, pos);
+        if (fl) {
+            Tree.add(strRes);
+        }
+    }
+    file.close();
+    return Tree;
 }
